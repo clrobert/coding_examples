16b189ef29aa4bac976de860290e631bb5766ade
Chris Roberts <clrobert@uno.edu>
2015-07-31 13:23:39 -0500

    k

--- game.rb
+++ game.rb
@@ -5,74 +5,104 @@ class Game
   def initialize names
     @names = names
     @names.shuffle!
+    @player_circle = PlayerCircle.new @names
 
-    @player_circle = PlayerCircle.new
-
-    @names.each do |name|
-      @player_circle.add Player.new name
-    end
+    @new_picker = @player_circle.select_random_player
+    @player_circle.remove @new_picker
+    @picker = Picker.new @new_picker.name
+    Announce.new_picker @picker
   end
 
   # Play a round of the game.
-  def play_round
-    @picker = @player_circle.select_random_player
-
-    @picker.save_circle = @player_circle.clone
+  def play_round   
+    @picker.save_circle @player_circle.clone
 
     goose = @picker.walk_circle
 
-    Announce.chase goose, @picker
+    # The goose comes out of the circle
+    @player_circle.remove goose
 
-    picker_caught = goose.chase
-    if !picker_caught      
+    Announce.goose_chase goose, @picker
+
+    picker_caught = goose.chase @picker
+
+    if !picker_caught   
+      # The old picker goes back into the circle   
+      @player_circle.push @picker
+
+      # The goose is the new picker
       @picker = Picker.new goose.name 
+
       Announce.new_picker @picker
+    else
+      # If the goose catches the picker, the goose goes back into the circle
+       @player_circle.push goose
+       Announce.old_picker @picker
     end
   end  
 end
 
 # Announces game events.
 class Announce
-  def self.chase player_a, player_b
+  def self.goose_chase player_a, player_b
     puts player_a.name + " now chases " + player_b.name + "!"
   end
 
   def self.new_picker player
-     player.name + " is now the picker!"
+     puts player.name + " is now the picker!"
+  end
+
+  def self.old_picker player    
+    puts player.name + " was caught! " + player.name + " is still the picker!"
   end
 end
 
 # Models both the actual player circle and the player's concept of a player circle.
 class PlayerCircle
-  attr_accessor :players, :full_list
+  attr_accessor :players, :roster
 
-  def reset
-    @players = @full_list
+  def initialize names   
+    @players = []
+    @roster = []
+
+    names.each do |name|
+      @players.push Player.new(name)
+      @roster.push Player.new(name)
+    end
+  end
+
+  def self.reset
+    @players = PlayerCircle.new(get_names(@roster.names))
   end
 
   def empty
     @players.size == 0
   end
 
+  def get_names players
+    array = players.map { |player| player.name }
+  end
+
   def clone
-    copy = PlayerCircle.new
-    copy.players = Array.new @players
+    copy = PlayerCircle.new(get_names(@players))
     copy 
   end
 
   def pop
-    players.pop
+    @players.pop
   end
 
-  def add
-    @players.append player
-    @full_list.append player
+  def remove player
+    @players.delete player
+  end
+
+  def push player
+    @players.push player
   end
 
   # Returns a random player from the list, no removal.
-  def self.select_random_player
-    generator = Random.new
-    random_number = generator.rand(@players.size) 
+  def select_random_player
+    random_number = (1..@players.size).to_a.sample 
     @players[random_number]
   end
 end
@@ -84,9 +114,54 @@ class Player
     @name = name
     @speed = (1..10).to_a.sample
   end
+end
+
+class Picker < Player
+  attr_accessor :chosen_goose, :my_circle
+
+  def save_circle circle    
+    @my_circle = []
+
+    chosen_goose = circle.select_random_player
+
+    circle.players.each do |player|
+      if player != chosen_goose
+        @my_circle.push Duck.new player.name
+      else
+        @my_circle.push Goose.new player.name
+      end
+    end
+  end
+
+  def is_this_the_goose? duck
+    puts duck.name + " is a " + duck.quack  
+    duck.can_chase  
+  end
+
+  def walk_circle
+    found_goose = false
+    goose = nil
+
+    while !found_goose
+      @my_circle.reset if @my_circle.empty?
+      player = @my_circle.pop
+      found_goose = is_this_the_goose? player
+      goose = player if found_goose
+    end
+
+    goose
+  end
+
+end
+
+class Duck < Player
 
   def self.quack
-    #puts self.class.to_s
+    self.class.to_s
+  end
+
+  def quack
+    self.class.to_s
   end
 
   def can_chase
@@ -94,64 +169,39 @@ class Player
   end
 end
 
-class Picker < Player
-  attr_accessor :chosen_goose, :my_circle
-
-  def save_circle circle
-    @my_circle = circle
-  end
-
-  def choose_goose
-    @chosen_goose = @my_circle.select_random_player
-  end
-
-  def dub player, chosen_goose, count, minimum_walk    
-    if (player.name == chosen_goose.name) && (minimum_walk < count)
-      label = "goose" 
-    else
-      label = "duck"
-    end
-
-    statement = player.name + " says " + label + " to " + player.name
-  end
+class Goose < Duck
+  attr_accessor :faking_it
 
   # One out of four times, pretend the goose isn't the goose.
-  def is_this_the_goose?
-    can_chase && [true, true, true, false].sample
+  def initialize name
+    super
+    @faking_it = [false, false, false, true].sample
   end
 
-  def walk_circle circle
-
-    choose_goose
-    found_goose = nil
-
-    while found_goose.nil?
-      player = @my_circle.pop
-      found_goose = is_this_the_goose?
-      @my_circle.reset if @my_circle.empty?
+  def quack
+    if faking_it
+      Duck.quack
+    else
+      super
     end
-
-    found_goose
   end
 
-end
-
-class Duck < Player
-end
-
-class Goose < Player
   def can_chase
-    true
+    able = true unless faking_it
+
+    # They should fake it at most one time.
+    faking_it = false
+
+    able
   end
+
   def chase picker
     @speed > picker.speed
   end
 end
 
-#the_game = Game.new ["Chris", "Shane", "Justin", "Luke", "Paulina", "David", "Ian", "Leigh", "Mattie", "Brice"]
+the_game = Game.new ["Chris", "Shane", "Justin", "Luke", "Paulina", "David", "Ian", "Leigh", "Mattie", "Brice"]
 
-#5.times do 
- # the_game.play_round
-#end
-
-Goose.quack
+3.times do 
+  the_game.play_round
+end
